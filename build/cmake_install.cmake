9b7f59d39a54fff47f075d73f13622ae4659f30a
anisa <anisamara777@gmail.com>
2024-11-06 23:20:06 +0100

    Ajout de joint.cpp et joint.hpp

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/nobil/Desktop/Projets/jeu_de_la_vie
+# Install script for directory: C:/Users/anisa/Documents/Game Of Life Project/game_of_life
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,13 +34,13 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/mingw32/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files (x86)/mingw32/bin/objdump.exe")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/nobil/Desktop/Projets/jeu_de_la_vie/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/anisa/Documents/Game Of Life Project/game_of_life/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/nobil/Desktop/Projets/jeu_de_la_vie/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/anisa/Documents/Game Of Life Project/game_of_life/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
