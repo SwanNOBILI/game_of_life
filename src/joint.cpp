afbce4b08b299bec260a7103ddfbf2233515efea
anisa <anisamara777@gmail.com>
2024-11-06 23:36:36 +0100

    rajout de "using namespace ..."

--- src/joint.cpp
+++ src/joint.cpp
@@ -1,13 +1,14 @@
 #include "../include/joint.hpp"
+using namespace std;
 
 Joint::Joint(double x, double y) : x(x), y(y), sx(0), sy(0) {}
 
 Joint::Joint(double x, double y, double sx, double sy) : x(x), y(y), sx(sx), sy(sy) {}
 
 double Joint::distance(const Joint& other) const {
-    return std::sqrt(std::pow(other.x - x, 2) + std::pow(other.y - y, 2));
+    return sqrt(pow(other.x - x, 2) + pow(other.y - y, 2));
 }
 
 void Joint::display() const {
-    std::cout << "(" << x << ", " << y << ")" << std::endl;
+    cout << "(" << x << ", " << y << ")" << endl;
 }
