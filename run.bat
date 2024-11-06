0f8de21ac5e368d8180b1d69829ed8615d2344eb
Platoooon <nobili.swan@gmail.com>
2024-11-06 21:19:53 +0100

    INIT

---
+++ run.bat
@@ -0,0 +1,39 @@
+@echo off
+
+rem Crée un dossier build s'il n'existe pas
+if not exist build (
+    mkdir build
+)
+cd build
+
+rem Génère les fichiers Make avec CMake
+echo Génération des fichiers Make avec CMake...
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=build ..
+if errorlevel 1 (
+    echo.
+    echo Erreur lors de la génération avec CMake.
+    exit /b
+)
+
+rem Compile le projet
+echo Compilation du projet...
+cmake --build .
+if errorlevel 1 (
+    echo.
+    echo Erreur lors de la compilation du projet.
+    rem Supprime le dossier build
+    rmdir /s /q build
+    exit /b
+)
+
+rem Vider le terminal
+cls
+
+rem Exécute l'exécutable
+bin\MonProjet.exe
+
+rem Passe à la ligne
+echo.
+
+rem Retourne au dossier racine
+cd ..
