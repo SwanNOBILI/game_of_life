510e980caabf8469451cd87c19f62ad3d6ea5f5c
Platoooon <nobili.swan@gmail.com>
2024-11-07 01:06:04 +0100

    Debug options

--- run.bat
+++ run.bat
@@ -1,12 +1,15 @@
 @echo off
 
+rem Supprime le dossier build
+rmdir /s /q build
+
 rem Crée un dossier build s'il n'existe pas
 if not exist build (
     mkdir build
 )
 cd build
 
-rem Génère les fichiers Make avec CMake
+rem Génère les fichiers Make avec CMake en mode build
 echo Génération des fichiers Make avec CMake...
 cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=build ..
 if errorlevel 1 (
@@ -15,17 +18,17 @@ if errorlevel 1 (
     exit /b
 )
 
-rem Compile le projet
+rem Vider le terminal
+cls
+
+rem Compile le projet avec affichage des erreurs en temps réel
 echo Compilation du projet...
-cmake --build .
+cmake --build . 2>&1
 if errorlevel 1 (
     echo.
     echo Erreur lors de la compilation du projet.
     exit /b
 )
-
-rem Vider le terminal
-cls
 
 rem Exécute l'exécutable
 bin\MonProjet.exe
